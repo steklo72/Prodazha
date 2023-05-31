c0349e3fde53d35f25c979ab9516c301f4fe84d3
Aleksandrfedotov <sasha4936@yandex.ru>
2023-05-31 17:03:23 +0500

    Initial Commit

---
+++ Prodazha/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Prodazha
+//
+//  Created by Fedotov Aleksandr on 31.05.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color("BackgroundColor").ignoresSafeArea()
+                VStack(spacing: 20) {
+                    // Logo
+                    Image("logo")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(20)
+                        .padding()
+
+                    // TextFields
+                    VStack(spacing: 15) {
+                        TextField("Email", text: $email)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                        SecureField("Password", text: $password)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    .padding(.horizontal)
+
+                    // Login Button
+//                    NavigationLink(destination: HomeView()) {
+//                        Text("Log in")
+//                            .frame(maxWidth: .infinity)
+//                            .font(.headline)
+//                            .foregroundColor(.white)
+//                            .padding()
+//                            .background(Color("MainColor"))
+//                            .cornerRadius(8)
+//                            .padding(.horizontal)
+//                    }
+
+                    Spacer()
+                }
+                .navigationTitle("Log in")
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ Prodazha/ProdazhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProdazhaApp.swift
+//  Prodazha
+//
+//  Created by Fedotov Aleksandr on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProdazhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
